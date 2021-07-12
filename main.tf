b8034bbfbec301fefc71cef8acf307537b2aca78
vidalherin <vidalherin@gmail.com>
2021-07-12 17:01:46 +0000

    added the first tag

--- main.tf
+++ main.tf
@@ -133,7 +133,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department  = devops
+    Department  = "devops"
+    Billable = true
   }
 }
 
