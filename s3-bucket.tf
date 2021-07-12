b32f43b880bf409746907e14de0e4640319e8422
vidalherin <vidalherin@gmail.com>
2021-07-12 19:08:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,10 @@
 module "s3-bucket" {
   source  = "app.terraform.io/Felix565-training/s3-bucket/aws"
   version = "2.2.0"
-  # insert required variabless here
-  bucket_prefix = "Matt_Hueffner"
+  bucket = "my-s3-bucket"
+  acl = "private"
+  versioning = {
+    enabled = true
+  }
+  bucket_prefix = MattHueffner
 }
